bc3c1b001ec7648721b999bd99ef5ba9f6e53a1f
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-03-06 22:17:14 -0300

    Update BarRoot.tsx

--- src/components/CommandBar/BarRoot.tsx
+++ src/components/CommandBar/BarRoot.tsx
@@ -16,31 +16,31 @@ export const BarRoot = () => {
 
   return (
     <>
-      <header className='h-screen min-w-14 max-w-14 bg-zinc-950 relative left-0 z-10 justify-between text-zinc-300'>
+      <header className='h-screen min-w-10 max-w-10 bg-zinc-950 relative left-0 z-10 justify-between text-zinc-300'>
         <div className='h-fit flex flex-col gap-5 items-center py-10'>
           <div className='flex gap-1'>
             <div className='size-1.5 bg-zinc-100 rounded-full' />
             <div className='size-1.5 bg-zinc-100/40 rounded-full' />
             <div className='size-1.5 bg-zinc-100/40 rounded-full' />
           </div>
-          <Sparkles className='size-6' />
+          <Sparkles className='md:size-6 size-5' />
         </div>
 
         <div className='flex flex-col justify-start items-center gap-5 pt-5 '>
           <Files
-            className='size-6 cursor-pointer'
+            className='md:size-6 size-5 cursor-pointer'
             onClick={() => {
               setOpenFiles(!openFiles)
             }}
           />
-          <LayoutGrid className='size-6 ' />
-          <Map className='size-6 ' />
+          <LayoutGrid className='md:size-6 size-5 ' />
+          <Map className='md:size-6 size-5 ' />
 
           {/* configuracao */}
-          <Settings2 className='size-6 ' />
+          <Settings2 className='md:size-6 size-5 ' />
         </div>
         {/* <div className=' flex flex-col justify-end items-center gap-5 pb-5 relative bottom-5'> */}
-        <LogOut className='size-6 absolute bottom-5 inset-x-4' />
+        <LogOut className='md:size-6 size-5 absolute bottom-5 inset-x-1/2 -translate-x-1/2' />
         {/* </div> */}
       </header>
 
