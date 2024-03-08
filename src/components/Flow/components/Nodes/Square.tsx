a6a54bfce54923231a7c11282dede7cb87d65b8d
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-03-07 21:33:04 -0300

    fiti: resizing nodes

--- src/components/Flow/components/Nodes/Square.tsx
+++ src/components/Flow/components/Nodes/Square.tsx
@@ -1,8 +1,30 @@
-import { NodeProps, Handle, Position } from 'reactflow'
+import { NodeProps, Handle, Position, NodeResizer } from 'reactflow'
 
-export const Square = (props: NodeProps) => {
+export const Square = ({ selected }: NodeProps) => {
   return (
-    <div className='bg-gradient-to-bl from-rose-700 to-pink-600 rounded-md w-[200px] h-[200px]'>
+    <div className='bg-gradient-to-bl from-rose-700 to-pink-600 rounded-md w-full h-full min-w-[150px] min-h-[150px]'>
+      <NodeResizer
+        minHeight={150}
+        minWidth={150}
+        isVisible={selected}
+        lineClassName=' border-sky-400'
+        handleClassName='h-3 w-3 bg-white border-2 rounded border-sky-400'
+      />
+
+      {/*
+      ?? test !
+      
+      <Handle
+        id='Bottom'
+        className={
+          selected
+            ? '-mb-1 size-3 bg-zinc-950 '
+            : '-mb-1 size-3 bg-zinc-950 invisible'
+        }
+        type='source'
+        position={Position.Bottom}
+      /> */}
+
       <Handle
         id='Bottom'
         className='-mb-1 size-3 bg-zinc-950 '
