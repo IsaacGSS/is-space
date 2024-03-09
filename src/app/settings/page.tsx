65f4835405d28e5735735bc374617d8fcad7570a
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-03-08 23:17:38 -0300

    roter settings

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section className='felx h-screen w-full flex bg-zinc-900'>
+      <div className='lg:w-1/4 md:w-1/3 w-1/2 h-full bg-zinc-950'></div>
+      <div>asdsadd</div>
+    </section>
+  )
+}
