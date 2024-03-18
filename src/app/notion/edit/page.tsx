be9e1b38c9f75897c19e493a6076ffa28a35638a
Isaac S. Silva <85599926+IsaacGSS@users.noreply.github.com>
2024-03-18 04:18:48 -0300

    Update page.tsx

---
+++ src/app/notion/edit/page.tsx
@@ -0,0 +1,11 @@
+import  {  Tiptap  }  de  '@/components/Tiptap'
+
+export default function Home ( ) {  
+  retornar  (
+    < >
+      < div  className = 'w-tela h completa flex bg-neutral-100 gap-2' >
+        < div  className = 'lg:w-1/4 md:w-1/3 w-0 h-full bg-zinc-200'  / >
+        < Tiptap  / >
+      < / div >
+    )
+  }
