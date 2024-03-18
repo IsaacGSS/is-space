e058d9e2621468edef6b37c34b9dda79416eb19a
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-03-18 19:33:36 -0300

    Update page.tsx

--- src/app/notion/edit/page.tsx
+++ src/app/notion/edit/page.tsx
@@ -1,12 +1,12 @@
-import  {  Tiptap  }  from  '@/components/Tiptap'
+import { Tiptap } from '@/components/Tiptap'
 
-export default function Home ( ) {  
-  return  (
-    < >
-      < div  className = 'w-full h-screen flex bg-neutral-100 gap-2' >
-        < div  className = 'lg:w-1/4 md:w-1/3 w-0 h-full bg-zinc-200'  / >
-        < Tiptap  / >
-      < / div >
-    </ >
-    )
-  }
+export default function Home() {
+  return (
+    <>
+      <div className='w-full h-screen flex bg-neutral-100 gap-2'>
+        <div className='lg:w-1/4 md:w-1/3 w-0 h-full bg-zinc-200' />
+        <Tiptap />
+      </div>
+    </>
+  )
+}
