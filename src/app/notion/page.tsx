0b1ecd43c752b1f5fe70b5956782c01ba1005cd2
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-03-21 18:44:08 -0300

    Update page.tsx

--- src/app/notion/page.tsx
+++ src/app/notion/page.tsx
@@ -1,4 +1,6 @@
-export default function Home() {
+import Link from 'next/link'
+
+export default function NodtionTiptap() {
   return (
     <>
       <section className='w-full h-screen p-5 flex flex-col bg-zinc-950 gap-2'>
@@ -10,23 +12,22 @@ export default function Home() {
           <div className='w-2/3 rounded-full shadow-xl drop-shadow-md shadow-zinc-950/10 h-10 border border-zinc-950'></div>
           <section className='flex flex-wrap items-end gap-10'>
             {Array.from({ length: 10 }).map((value, index) => (
-              <div
-                key={index}
-                className='size-56 border border-zinc-900 rounded-md shadow-md shadow-black/20 overflow-hidden p-3 text-justify '
-              >
-                <span className='text-lg font-medium capitalize'>Title</span>
-                <br />
-                <span className='text-sm align-middle indent-3'>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Ratione asperiores repudiandae earum autem nesciunt
-                  exercitationem! Molestiae, harum quia iste eaque neque nisi
-                  alias voluptate, obcaecati pariatur corporis soluta,
-                  distinctio iusto. Neque ut corrupti iure in, dignissimos id
-                  quia vitae. Dolorum ex ipsam sit in nemo ad magnam quasi
-                  incidunt consequuntur neque. Veniam hic accusamus mollitia at.
-                  Odit eum possimus dolorem.
-                </span>
-              </div>
+              <Link key={index} href={'/notion/edit'}>
+                <div className='size-56 border border-zinc-900 rounded-md shadow-md shadow-black/20 overflow-hidden p-3 text-justify '>
+                  <span className='text-lg font-medium capitalize'>Title</span>
+                  <br />
+                  <span className='text-sm align-middle indent-3'>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Ratione asperiores repudiandae earum autem nesciunt
+                    exercitationem! Molestiae, harum quia iste eaque neque nisi
+                    alias voluptate, obcaecati pariatur corporis soluta,
+                    distinctio iusto. Neque ut corrupti iure in, dignissimos id
+                    quia vitae. Dolorum ex ipsam sit in nemo ad magnam quasi
+                    incidunt consequuntur neque. Veniam hic accusamus mollitia
+                    at. Odit eum possimus dolorem.
+                  </span>
+                </div>
+              </Link>
             ))}
           </section>
         </section>
