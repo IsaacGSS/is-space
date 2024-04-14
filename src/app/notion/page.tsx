09901358b8081dd6a1cd1acdb71d53fd8729e82f
Isaac S. Silva <85599926+IsaacGSS@users.noreply.github.com>
2024-04-14 08:21:47 +0000

    up new layout

--- src/app/notion/page.tsx
+++ src/app/notion/page.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link'
 export default function NodtionTiptap() {
   return (
     <>
-      <section className='w-full max-h-screen p-5 pb-10  flex flex-col bg-zinc-950 gap-2'>
-        <section className='h-20 w-full text-neutral-50 inline-flex items-center'>
+      <section className='w-full max-h-screen flex flex-col bg-zinc-950 gap-2'>
+        <section className='h-20 w-full text-neutral-50 inline-flex items-center pl-5'>
           <span className='text-2xl font-inter'>Project Kabum</span>
         </section>
 
-        <section className='h-4/5 p-10 px-0 bg-neutral-100 w-full rounded-3xl overflow-hidden'>
-          <div className='w-2/3 rounded-full shadow-xl drop-shadow-md shadow-zinc-950/10 h-10 border border-zinc-950 '></div>
-          <section className='h-full p-5 flex flex-wrap items-end gap-10 overflow-y-scroll'>
+        <section className='h-4/5 pl-5 pt-5 bg-neutral-100 w-full  overflow-hidden'>
+          <div className='h-10 md:w-2/3 w-full md:p-0 px-5  rounded-full shadow-xl drop-shadow-md shadow-zinc-950/10  border border-zinc-950 '></div>
+          <section className='h-full mt-2 p-5 pb-14 flex flex-wrap items-end gap-10 overflow-y-scroll'>
             {Array.from({ length: 20 }).map((value, index) => (
               <Link key={index} href={'/notion/edit'}>
                 <div className='size-56 border border-zinc-900 rounded-md shadow-md shadow-black/20 overflow-hidden p-3 text-justify '>
